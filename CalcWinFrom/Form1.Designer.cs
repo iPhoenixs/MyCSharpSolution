101af4acf7bab2244f8fc4e67ca1e5819c932128
iPhoenixs <t_huricane@hotmail.com>
2015-01-29 11:03:29 +0700

    2nd Commit

--- CalcWinFrom/Form1.Designer.cs
+++ CalcWinFrom/Form1.Designer.cs
@@ -42,12 +42,16 @@
             // 
             // lblResult
             // 
+            this.lblResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblResult.AutoSize = true;
-            this.lblResult.Location = new System.Drawing.Point(12, 61);
+            this.lblResult.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblResult.Location = new System.Drawing.Point(7, 98);
             this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.Size = new System.Drawing.Size(22, 25);
             this.lblResult.TabIndex = 1;
-            this.lblResult.Text = "label1";
+            this.lblResult.Text = "?\r\n";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // button1
             // 
@@ -55,7 +59,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Calc";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -63,12 +67,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(351, 82);
+            this.ClientSize = new System.Drawing.Size(351, 132);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
